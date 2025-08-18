b92095808099907cc43e0abd1f727cd20b8b6b3e
flacko <sundaaay020202@gmail.com>
2025-08-18 16:34:45 +0600

    Add migration

---
+++ src/BS.Infrastructure/Data/Migrations/20250818084805_ChangePropInBooking.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BS.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePropInBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_AspNetUsers_UserId1",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_UserId1",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Bookings",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_AspNetUsers_UserId",
+                table: "Bookings",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_AspNetUsers_UserId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Bookings",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Bookings",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId1",
+                table: "Bookings",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_AspNetUsers_UserId1",
+                table: "Bookings",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
