307c9eb0540714ff308bb39729aecfa25cc8272d
flacko <sundaaay020202@gmail.com>
2025-08-27 13:57:35 +0600

    Refactor Booking domain

--- src/BS.Domain/Entities/Booking.cs
+++ src/BS.Domain/Entities/Booking.cs
@@ -1,20 +1,51 @@
-using BS.Domain.Enums;
+using BS.Domain.Entities;
 
-namespace BS.Domain.Entities;
-
-public class Booking: BaseEntity
+public class Booking : BaseEntity
 {
-    public int Id { get; set; }
-    
-    public string UserId { get; set; }
-    public User User { get; set; }
-    
-    public int ResourceId { get; set; }
-    public Resource Resource { get; set; }
-    
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
-    public DateTime CreatedAt { get; set; }
-    
-    public BookingStatus Status { get; set; }
+    public int Id { get; private set; }
+
+    public int SpotId { get; private set; }
+    public Spot Spot { get; private set; }
+
+    public string UserId { get; private set; }
+    public User User { get; private set; }
+
+    public DateTime CreatedAt { get; private set; }
+    public BookingStatus Status { get; private set; }
+
+    private Booking() { }
+
+    private Booking(Spot spot, string userId)
+    {
+        if (spot == null)
+            throw new ArgumentNullException(nameof(spot));
+
+        if (!spot.Resource.IsActive)
+            throw new InvalidOperationException("Resource is not active.");
+
+        SpotId = spot.Id;
+        UserId = userId;
+        CreatedAt = DateTime.UtcNow;
+        Status = BookingStatus.Active;
+    }
+
+    public static Booking Create(Spot spot, string userId)
+    {
+        return new Booking(spot, userId);
+    }
+
+    public void Cancel()
+    {
+        if (Status != BookingStatus.Active)
+            throw new InvalidOperationException("Unable to Cancel Booking.");
+
+        Status = BookingStatus.Canceled;
+    }
+}
+
+public enum BookingStatus
+{
+    Active,
+    Completed,
+    Canceled
 }
